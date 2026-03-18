e71a056325e99ecf1100cf9b279b5f9edc6da26a
itsssanu <anuperumal153@gmail.com>
2026-03-18 21:47:40 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -14,3 +14,7 @@ app.use("/api", mailRoutes);
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
+
+app.get("/", (req, res) => {
+  res.send("🚀 Portfolio API is running...");
+});
