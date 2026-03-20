9cecc527e87539c2f0a4cb845874bc64e85c2e6f
itsssanu <anuperumal153@gmail.com>
2026-03-20 21:21:26 +0530

    mail config changes

--- server/config/mailConfig.js
+++ server/config/mailConfig.js
@@ -1,6 +1,6 @@
 const nodemailer = require("nodemailer");
 
-const transporter = nodemailer.createTransporter({
+const transporter = nodemailer.createTransport({
   host: "smtp.resend.com",
   port: 465,
   secure: true,
