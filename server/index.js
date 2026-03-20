fa5b128cda12fe746daddfb49002ed39145b0cb3
itsssanu <anuperumal153@gmail.com>
2026-03-20 14:14:49 +0530

    Revert "changes"
    This reverts commit e71a056325e99ecf1100cf9b279b5f9edc6da26a.

--- server/index.js
+++ server/index.js
@@ -14,7 +14,3 @@ app.use("/api", mailRoutes);
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
-
-app.get("/", (req, res) => {
-  res.send("🚀 Portfolio API is running...");
-});
