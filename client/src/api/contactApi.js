11c916c1307c93599f8b2feb5fc1ccc95db456d9
itsssanu <anuperumal153@gmail.com>
2026-03-20 14:57:20 +0530

    initial commit

--- client/src/api/contactApi.js
+++ client/src/api/contactApi.js
@@ -1,11 +1,10 @@
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+
 export const sendContactMail = async (formData) => {
-  const res = await fetch("http://localhost:5000/api/send-mail", {
+  const res = await fetch(`${BASE_URL}/api/send-mail`, {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify(formData),
   });
-
   return res.json();
 };
